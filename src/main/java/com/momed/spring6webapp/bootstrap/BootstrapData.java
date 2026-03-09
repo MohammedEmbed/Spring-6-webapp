c4948890dae57e6ac11fd2d804af8c0ef3eca382
Mohammed <m.hamdoune8@gmail.com>
2026-03-09 18:03:22 +0100

    h2 database configuration

--- src/main/java/com/momed/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/com/momed/spring6webapp/bootstrap/BootstrapData.java
@@ -24,6 +24,7 @@ public class BootstrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
         Author ted = new Author();
         ted.setLastName("Kaczynski");
         ted.setFirstName("Ted");
@@ -32,39 +33,41 @@ public class BootstrapData implements CommandLineRunner {
         isaif.setTitle("Industrial Society and Its Future");
         isaif.setIsbn("1");
 
+        Author eric = new Author();
+        eric.setLastName("Eric");
+        eric.setFirstName("Evans");
+
+        Book ddd = new Book();
+        ddd.setTitle("Domain Driven Design");
+        ddd.setIsbn("2");
+
         Author tedSaved = authorRepository.save(ted);
         Book isaifSaved = bookRepository.save(isaif);
+        Author ericSaved = authorRepository.save(eric);
+        Book dddSaved = bookRepository.save(ddd);
 
         Publisher tedpub = new Publisher();
+        tedpub.setPublisherName("Teddy Publishing");
         tedpub.setAddress("1378 Stemple Pass Rd");
         tedpub.setCity("Lincoln");
         tedpub.setState("MT");
         tedpub.setZip("59639");
         Publisher tedpubSaved = publisherRepository.save(tedpub);
 
-        Author eric = new Author();
-        ted.setLastName("Eric");
-        ted.setFirstName("Evans");
-
-        Book ddd = new Book();
-        isaif.setTitle("Domain Driven Design");
-        isaif.setIsbn("2");
-
-        Author ericSaved = authorRepository.save(eric);
-        Book dddSaved = bookRepository.save(ddd);
 
         isaifSaved.setPublisher(tedpubSaved);
         dddSaved.setPublisher(tedpubSaved);
 
-        bookRepository.save(dddSaved);
-        bookRepository.save(isaifSaved);
 
         tedSaved.getBooks().add(isaifSaved);
         ericSaved.getBooks().add(dddSaved);
+        isaifSaved.getAuthors().add(tedSaved);
+        dddSaved.getAuthors().add(ericSaved);
 
         authorRepository.save(ericSaved);
         authorRepository.save(tedSaved);
-
+        bookRepository.save(dddSaved);
+        bookRepository.save(isaifSaved);
 
         System.out.println("In Bootstrap");
         System.out.println("Author Count: " + authorRepository.count());
